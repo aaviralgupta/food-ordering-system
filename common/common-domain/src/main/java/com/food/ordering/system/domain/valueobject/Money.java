7b0f16e48707bb4c42af6230d96d5a9b1959efb7
Aviral Gupta <aaviralgupta@gmail.com>
2023-04-30 23:07:47 +0530

    Created methods related to initialze & validate order

--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 public class Money {
     private final BigDecimal amount;
 
+    public static final Money ZERO = new Money(BigDecimal.ZERO);
+
     public Money(BigDecimal amount) {
         this.amount = setScale(amount);
     }
@@ -27,8 +29,8 @@ public class Money {
         return new Money(setScale(this.amount.subtract(money.getAmount())));
     }
 
-    public Money multiply(Money money){
-        return new Money(setScale(this.amount.multiply(money.getAmount())));
+    public Money multiply(int multiplier){
+        return new Money(setScale(this.amount.multiply(new BigDecimal(multiplier))));
     }
 
     public boolean isGreaterThan(Money money){
--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
@@ -20,6 +20,17 @@ public class OrderItem extends BaseEntity<OrderItemId> {
         subTotal = builder.subTotal;
     }
 
+    boolean isPriceValid(){
+        return price.isGreaterThanZero() &&
+                price.equals(product.getPrice()) &&
+                price.multiply(quantity).equals(subTotal);
+    }
+
+    void initializeOrderItem(OrderId orderId, OrderItemId orderItemId){
+        this.orderId= orderId;
+        super.setId(orderItemId);
+    }
+
     public static Builder builder(){
         return new Builder();
     }
