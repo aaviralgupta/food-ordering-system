c957644a174619245ff75531c794635e45f76156
Aviral Gupta <aaviralgupta@gmail.com>
2023-05-02 00:52:07 +0530

    Created CreateOrderCommand and related classes

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/domain/dto/create/CreateOrderCommand.java
@@ -0,0 +1,26 @@
+package com.food.ordering.system.domain.dto.create;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateOrderCommand {
+    @NotNull
+    private final UUID customerId;
+    @NotNull
+    private final UUID restaurantId;
+    @NotNull
+    private final BigDecimal price;
+    @NotNull
+    private final List<OrderItem> orderItems;
+    @NotNull
+    private final OrderAddress address;
+}
