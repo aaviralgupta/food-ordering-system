281d96a3d969e3a2fbec128db118157a5ffe861c
Aviral Gupta <aaviralgupta@gmail.com>
2023-05-02 19:09:35 +0530

    Created impl classes for Payment Response and Restraunt Approval message listner

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/order/system/domain/RestrauntApprovalResponseMessageListenerImpl.java
@@ -0,0 +1,22 @@
+package com.food.ordering.order.system.domain;
+
+import com.food.ordering.order.system.domain.dto.message.RestaurantApprovalResponse;
+import com.food.ordering.order.system.domain.ports.input.message.listener.restaurantapproval.RestaurantApprovalResponseMessageListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Slf4j
+@Validated
+@Service
+public class RestrauntApprovalResponseMessageListenerImpl implements RestaurantApprovalResponseMessageListener {
+    @Override
+    public void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse) {
+
+    }
+
+    @Override
+    public void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse) {
+
+    }
+}
--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/order/system/domain/mapper/OrderDataMapper.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/order/system/domain/mapper/OrderDataMapper.java
@@ -3,6 +3,7 @@ package com.food.ordering.order.system.domain.mapper;
 import com.food.ordering.order.system.domain.dto.create.CreateOrderCommand;
 import com.food.ordering.order.system.domain.dto.create.CreateOrderResponse;
 import com.food.ordering.order.system.domain.dto.create.OrderAddress;
+import com.food.ordering.order.system.domain.dto.track.TrackOrderResponse;
 import com.food.ordering.order.system.domain.valueobject.CustomerId;
 import com.food.ordering.order.system.domain.valueobject.Money;
 import com.food.ordering.order.system.domain.valueobject.ProductId;
@@ -11,11 +12,9 @@ import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.entity.OrderItem;
 import com.food.ordering.system.order.service.domain.entity.Product;
 import com.food.ordering.system.order.service.domain.entity.Restaurant;
-import com.food.ordering.system.order.service.domain.valueobject.OrderItemId;
 import com.food.ordering.system.order.service.domain.valueobject.StreetAddress;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -41,6 +40,21 @@ public class OrderDataMapper {
                 build();
     }
 
+    public CreateOrderResponse orderToCreateOrderResponse(Order order) {
+        return CreateOrderResponse.builder()
+                .orderTrackingId(order.getTrackingId().getValue())
+                .orderStatus(order.getOrderStatus())
+                .build();
+    }
+
+    public TrackOrderResponse orderToTrackOrderResponse(Order order){
+        return TrackOrderResponse.builder()
+                .orderTrackingId(order.getTrackingId().getValue())
+                .orderStatus(order.getOrderStatus())
+                .failureMessages(order.getFailureMessages())
+                .build();
+    }
+
     private List<OrderItem> orderItemsToOrderItemEntities(List<com.food.ordering.order.system.domain.dto.create.OrderItem> orderItems) {
         return orderItems.stream().map(orderItem ->
                 OrderItem.builder()
@@ -60,10 +74,5 @@ public class OrderDataMapper {
         );
     }
 
-    public CreateOrderResponse orderToCreateOrderResponse(Order order) {
-        return CreateOrderResponse.builder()
-                .orderTrackingId(order.getTrackingId().getValue())
-                .orderStatus(order.getOrderStatus())
-                .build();
-    }
+
 }
