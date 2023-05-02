0cabe3ef3bcd539b62659f53d80cb77ce39890cb
Aviral Gupta <aaviralgupta@gmail.com>
2023-05-02 19:20:09 +0530

    Code Refactoring

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/order/system/domain/ports/input/message/listener/restaurantapproval/RestaurantApprovalResponseMessageListener.java
+++
@@ -1,9 +0,0 @@
-package com.food.ordering.order.system.domain.ports.input.message.listener.restaurantapproval;
-
-import com.food.ordering.order.system.domain.dto.message.RestaurantApprovalResponse;
-
-public interface RestaurantApprovalResponseMessageListener {
-
-    void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse);
-    void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse);
-}
---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -0,0 +1,38 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
+import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Slf4j
+@Component
+public class OrderTrackCommandHandler {
+
+    private final OrderDataMapper orderDataMapper;
+
+    private final OrderRepository orderRepository;
+
+    public OrderTrackCommandHandler(OrderDataMapper orderDataMapper, OrderRepository orderRepository) {
+        this.orderDataMapper = orderDataMapper;
+        this.orderRepository = orderRepository;
+    }
+
+    @Transactional(readOnly = true)
+    TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery){
+        Optional<Order> order = orderRepository.findByTrackingId(new TrackingId(trackOrderQuery.getOrderTrackingId()));
+        if(order.isEmpty()){
+            log.warn("Could not find order with id : {}",trackOrderQuery.getOrderTrackingId());
+            throw new OrderNotFoundException("Could not find order with id : "+trackOrderQuery.getOrderTrackingId());
+        }
+        return orderDataMapper.orderToTrackOrderResponse(order.get());
+    }
+}
