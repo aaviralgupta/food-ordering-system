be64e67dcd2265b117172430d6efb8135a53a842
Aviral Gupta <aaviralgupta@gmail.com>
2023-06-28 13:17:17 +0530

    Created common-dataaccess module and moved restaurant related classes there

--- order-service/order-dataacces/src/main/java/com/food/ordering/system/order/service/dataaccess/restaurant/mapper/RestaurantDataAccessMapper.java
+++ order-service/order-dataacces/src/main/java/com/food/ordering/system/order/service/dataaccess/restaurant/mapper/RestaurantDataAccessMapper.java
@@ -1,7 +1,7 @@
 package com.food.ordering.system.order.service.dataaccess.restaurant.mapper;
 
-import com.food.ordering.system.order.service.dataaccess.restaurant.entity.RestaurantEntity;
-import com.food.ordering.system.order.service.dataaccess.restaurant.exception.RestaurantDataAccessException;
+import com.food.ordering.system.dataaccess.restaurant.entity.RestaurantEntity;
+import com.food.ordering.system.dataaccess.restaurant.exception.RestaurantDataAccessException;
 import com.food.ordering.system.order.service.domain.entity.Product;
 import com.food.ordering.system.order.service.domain.entity.Restaurant;
 import com.food.ordering.system.order.service.domain.valueobject.Money;
