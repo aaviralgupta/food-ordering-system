9c2d8eb83af7c676481ff55af02ef97aca82c5b4
Aviral Gupta <aaviralgupta@gmail.com>
2023-07-18 00:33:16 +0530

    Created scheduler class for publishing payment responses

--- payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/output/repository/OrderOutboxRepository.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/output/repository/OrderOutboxRepository.java
@@ -4,13 +4,14 @@ import com.food.ordering.system.domain.valueobject.PaymentStatus;
 import com.food.ordering.system.outbox.OutboxStatus;
 import com.food.ordering.system.payment.service.domain.outbox.model.OrderOutboxMessage;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderOutboxRepository {
 
     OrderOutboxMessage save(OrderOutboxMessage orderOutboxMessage);
-    Optional<OrderOutboxMessage> findByTypeAndOutboxStatus(String type, OutboxStatus status);
+    Optional<List<OrderOutboxMessage>> findByTypeAndOutboxStatus(String type, OutboxStatus status);
     Optional<OrderOutboxMessage> findByTypeAndSagaIdAndPaymentStatusAndOutboxStatus(String type,
                                                                                     UUID sagaId,
                                                                                     PaymentStatus paymentStatus,
