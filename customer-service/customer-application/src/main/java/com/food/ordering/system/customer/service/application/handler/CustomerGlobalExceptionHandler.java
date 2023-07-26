0b91f133015e09fb48d0fef914e1eec288ea42f6
Aviral Gupta <aaviralgupta@gmail.com>
2023-07-26 08:34:40 +0530

    customer controller implementation

---
+++ customer-service/customer-application/src/main/java/com/food/ordering/system/customer/service/application/handler/CustomerGlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.food.ordering.system.customer.service.application.handler;
+
+import com.food.ordering.system.application.handler.ErrorDTO;
+import com.food.ordering.system.application.handler.GlobalExceptionHandler;
+import com.food.ordering.system.customer.service.domain.exception.CustomerDomainException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@Slf4j
+@ControllerAdvice
+public class CustomerGlobalExceptionHandler extends GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = {CustomerDomainException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleException(CustomerDomainException customerDomainException){
+        log.error(customerDomainException.getMessage(), customerDomainException);
+        return ErrorDTO.builder()
+                .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message(customerDomainException.getMessage())
+                .build();
+    }
+}
